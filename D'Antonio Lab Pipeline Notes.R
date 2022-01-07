688e10ce833a2c72a316d7c14bb3e59f7adb2992
zoeisabellafung <funggzoee@gmail.com>
2022-01-06 23:38:30 -0800

    initial commit/load datasets into r

---
+++ D'Antonio Lab Pipeline Notes.R
@@ -0,0 +1,20 @@
+# goals for week 1
+    # read in datasheets
+    # create outline of shiny app
+
+    # outline for shiny app
+      # for biomass: can choose which location to look at, or compare avg. values between species
+          # max, min, range, mean, median, sd., histogram
+          # correlation b/w location and mass
+      # for branches: can choose which species/transect to look at, or compare avg. values between species/transects
+          # max, min, range, mean, median, sd., histogram
+          # correlations b/w transect/species/branch length/ramification
+      # for leaf traits: can choose which species/transect to look at, or compare avg. values between species/transect
+          # max, min, range, mean, median, sd., histogram
+          # correlations b/w transect/species/leaf curling/leaf thickness/mass
+
+# questions for week 1 meeting
+  # are all of the data going to be similar enough where they can use the same sorts of visualization?
+  # or should we just include a bunch of visualization options?
+  # what does "ramification" in the branches .csv refer to?
+      
