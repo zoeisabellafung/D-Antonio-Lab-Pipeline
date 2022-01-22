6b929e3c85bdc04efa96c09056524f1e4aace7fe
zoeisabellafung <funggzoee@gmail.com>
2022-01-22 12:19:03 -0800

    developed graph transect x leaf curling & added checkbox group to filter graph by transect

--- DAntonioShinyapp.R
+++ DAntonioShinyapp.R
@@ -10,18 +10,21 @@ library(ggplot2)
 library(dplyr)
 library(forcats)
 
-leaf_traits <- read_csv("leaf_traits.csv") # load Midland leaf traits data
+leaf_traits <- read_csv("leaf_traits_shiny.csv") # load Midland leaf traits data
+leaf_traits$transect <- as.factor(leaf_traits$transect) # make "transect" a factor
+# leaf_traits$transect <- unlist(leaf_traits$transect) # make "transect" not a list
 
 ui <- fluidPage(theme=shinytheme("darkly"), # set theme of ui
                 titlePanel("Midland Leaf Traits"), # set title of page
-                sidebarLayout( # create a side bar
+                sidebarLayout( # create a side bar/main panel layout
                   sidebarPanel("Data Visualization", # title the side bar 
-                               radioButtons(inputId="transect", # create a radio buttons widget that allows users to filter data by species
-                                            label="Select a transect to view:",
-                                            choices=c(unique(leaf_traits$transect))),
-                               radioButtons(inputId="species",
-                                            label="Select a species to view:",
-                                            choices=c(unique(leaf_traits$species_cleaned)))),
+                               checkboxGroupInput(inputId="transect", # create a checkbox widget that allows users to filter data by transect
+                                            label="Select transect(s) to view:",
+                                            choices=c(unique(leaf_traits$transect)),
+                                            selected=c(unique(leaf_traits$transect)))), # all automatically selected when app is first run
+                               # radioButtons(inputId="species",
+                               #              label="Select a species to view:",
+                               #              choices=c(unique(leaf_traits$species_cleaned)))),
                   mainPanel(
                     h4("Summary"),
                     verbatimTextOutput("table"),
@@ -33,17 +36,15 @@ ui <- fluidPage(theme=shinytheme("darkly"), # set theme of ui
 server <- function(input,output){
   leaf_traits_reactive <- reactive({ # create a reactive data frame for leaf traits data
     leaf_traits %>%
-      as.character(transect) # make transect a character vector
-      mutate(transect=fct_reorder(transect,"1","2","3","4","6","7","10","12")) %>% # reorder transect by numerical order
-      filter(transect==input$transect) %>% # filter the data set by the transect input
-      filter(species_cleaned==input$species) # filter the data set by the species input
+      filter(transect==input$transect) # filter the data set by the transect input
+      # filter(species_cleaned==input$species) # filter the data set by the species input
   })
   output$table <- renderTable({
     head(data=leaf_traits_reactive(), n = isolate(input$transect))
   })
   output$leaf_traits_plot <- renderPlot({
     ggplot(data=leaf_traits_reactive(),aes(x=transect,y=curling_mm,color=species_cleaned,shape=transect)) +
-      geompoint()
+      geom_point()
   })
 } 
 
