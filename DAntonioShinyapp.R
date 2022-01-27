7f9508bc7e67084b103b1958fcec021ea33a6dd1
zoeisabellafung <funggzoee@gmail.com>
2022-01-26 21:54:32 -0800

    create tabs, add cond. panel to filter graph by species

--- DAntonioShinyapp.R
+++ DAntonioShinyapp.R
@@ -9,41 +9,75 @@ library(shinythemes)
 library(ggplot2)
 library(dplyr)
 library(forcats)
+library(bslib)
 
-leaf_traits <- read_csv("leaf_traits_shiny.csv") # load Midland leaf traits data
-leaf_traits$transect <- as.factor(leaf_traits$transect) # make "transect" a factor
+leaf_traits_shiny <- read_csv("leaf_traits_shiny.csv") # load Midland leaf traits data
+# leaf_traits_shiny$transect <- as.factor(leaf_traits_shiny$transect) # make "transect" a factor
+# mutate(leaf_traits$transect=fct_reorder(transect,"1","2","3","4","6","7","10","12")) # reorder transect by numerical order
+# levels(leaf_traits_shiny$transect) <- c("1","2","3","4","6","7","10","12") # rename the transect levels 
 # leaf_traits$transect <- unlist(leaf_traits$transect) # make "transect" not a list
 
-ui <- fluidPage(theme=shinytheme("darkly"), # set theme of ui
-                titlePanel("Midland Leaf Traits"), # set title of page
-                sidebarLayout( # create a side bar/main panel layout
-                  sidebarPanel("Data Visualization", # title the side bar 
-                               checkboxGroupInput(inputId="transect", # create a checkbox widget that allows users to filter data by transect
-                                            label="Select transect(s) to view:",
-                                            choices=c(unique(leaf_traits$transect)),
-                                            selected=c(unique(leaf_traits$transect)))), # all automatically selected when app is first run
-                               # radioButtons(inputId="species",
-                               #              label="Select a species to view:",
-                               #              choices=c(unique(leaf_traits$species_cleaned)))),
-                  mainPanel(
-                    h4("Summary"),
-                    verbatimTextOutput("table"),
-                    h4("Graphical output"),
-                            plotOutput(outputId="leaf_traits_plot"), #create a scatterplot in main panel of leaf thickness per species across all transects
-                            ), 
-                ))
+species_summary <- leaf_traits_shiny$species_cleaned # create vector
+
+ui <- navbarPage("D'Antonio Lab Shiny app", 
+                 theme=bs_theme( # set custom theme
+                   bg="",
+                   fg="",
+                   primary="",
+                   base_font=(""),
+                   code_font=("")
+                 ), # set theme of ui
+                 
+                 # tab 1
+                 tabPanel("Summary Statistics", # create tab for summary stats/overview of data
+                          fluidRow(column(12,
+                                          h1("Overview of Midland Leaf Traits Data")), # header of the tab page
+                                   br(), # add a line of space
+                                   br() # add another line of space
+                          ),
+                          fluidRow(column(12, align="center",
+                                          h3("Midland Leaf Traits Summary Statistics"),
+                                          verbatimTextOutput("summary_table"), # display table with summary statistics
+                                          radioButtons(inputId="species",
+                                                       label="Select a species to view:",
+                                                       choices=c(unique(leaf_traits_shiny$species_cleaned))))
+                                          )),
+                 
+                 # tab 2
+                 tabPanel("Visualization", # create tab for data visualization
+                          h1("Visualizing Midland Leaf Traits"), # header of the tab page
+                          sidebarLayout( # create a side bar/main panel layout
+                            sidebarPanel("Filter by transect and species", # title the side bar
+                                         checkboxGroupInput(inputId="transect", # create a checkbox widget that allows users to filter data by transect
+                                                            label="Select transect(s) to view:",
+                                                            choices=c(unique(leaf_traits_shiny$transect)),
+                                                            selected=c(unique(leaf_traits_shiny$transect))), # all automatically selected when app is first run
+                                         conditionalPanel( # only show this panel if transect 1 is selected
+                                           condition="input.transect=='1'",
+                                           checkboxGroupInput(inputId="cond_species_1", # checkbox widget that allows users to filter the transect data by species
+                                                              label="Select species to view:",
+                                                              choices=c(unique(leaf_traits_shiny$species_cleaned[leaf_traits_shiny$transect==1])), # only the species in transect one are choices
+                                                              selected=c(unique(leaf_traits_shiny$species_cleaned[leaf_traits_shiny$transect==1]))))), # automatically select all choices
+                            mainPanel(
+                              h4("Graphical output"),
+                              plotOutput(outputId="leaf_traits_plot") #create a scatterplot in main panel of leaf thickness per species across all transects
+                            ) 
+                )))
 
 server <- function(input,output){
-  leaf_traits_reactive <- reactive({ # create a reactive data frame for leaf traits data
-    leaf_traits %>%
+  leaf_traits_reactive <- reactive({ # create a reactive data frame for leaf traits data visualization
+    leaf_traits_shiny %>%
       filter(transect==input$transect) # filter the data set by the transect input
-      # filter(species_cleaned==input$species) # filter the data set by the species input
   })
-  output$table <- renderTable({
-    head(data=leaf_traits_reactive(), n = isolate(input$transect))
+  leaf_traits_reactive2 <- reactive({
+    leaf_traits_shiny %>%
+      filter(species_cleaned==input$species) # filter the data set by the species input
+  })
+  output$summary_table <- renderPrint({ # create a reactive table of summary stats
+    summary(leaf_traits_reactive[,as.numeric(input$species)]) 
   })
   output$leaf_traits_plot <- renderPlot({
-    ggplot(data=leaf_traits_reactive(),aes(x=transect,y=curling_mm,color=species_cleaned,shape=transect)) +
+    ggplot(data=leaf_traits_reactive(),aes(x=transect,y=curling_mm,color=species_cleaned)) +
       geom_point()
   })
 } 
